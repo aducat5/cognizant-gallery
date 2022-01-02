ef12671357e38cb3885d12695dd1a8ee34e153a4
Tamer Berat Çelik <aducat5@hotmail.com>
2022-01-02 19:33:03 +0300

    added sample service to get

---
+++ CognizantGallery.Web.Api/CognizantGallery.Data/Services/WarehouseService.cs
@@ -0,0 +1,26 @@
+﻿using CognizantGallery.Model;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognizantGallery.Data.Product
+{
+    public class WarehouseService
+    {
+        private readonly IMongoCollection<Warehouse> _warehouses;
+
+        public WarehouseService(ICognizantGallerySettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            var _warehouses = database.GetCollection<Vehicle>(settings.WarehouseCollectionName);
+        }
+        public async Task<List<Warehouse>> GetAllAsync()
+        {
+            return await _warehouses.Find(s => true).ToListAsync();
+        }
+    }
+}
