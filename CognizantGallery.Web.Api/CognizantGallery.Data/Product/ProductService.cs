ff805097a711d0c0cf9e1510edb6831703273cc4
Tamer Berat Çelik <aducat5@hotmail.com>
2022-01-02 19:30:55 +0300

    moved db connection to data project

---
+++ CognizantGallery.Web.Api/CognizantGallery.Data/Product/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using CognizantGallery.Model;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognizantGallery.Data.Product
+{
+    public class ProductService
+    {
+        private readonly IMongoCollection<Vehicle> _vehicles;
+
+        public ProductService(ICognizantGallerySettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            var warehouses = database.GetCollection<Vehicle>(settings.WarehouseCollectionName);
+        }
+        public async Task<List<Vehicle>> GetAllAsync()
+        {
+            return await _vehicles.Find(s => true).ToListAsync();
+        }
+        public async Task<Vehicle> GetByIdAsync(string id)
+        {
+            return await _vehicles.Find<Vehicle>(s => s.Id == id).FirstOrDefaultAsync();
+        }
+
+    }
+}
