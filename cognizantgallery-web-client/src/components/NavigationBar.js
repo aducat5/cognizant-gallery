b9ea5f148ea58845934c5521abdc4c05a56d53bf
Tamer Berat Çelik <aducat5@hotmail.com>
2022-01-03 00:21:45 +0300

    added some extra styling and axios

--- cognizantgallery-web-client/src/components/NavigationBar.js
+++ cognizantgallery-web-client/src/components/NavigationBar.js
@@ -1,35 +1,32 @@
-import { Children, ReactNode } from 'react';
-import {
-  Box,
-  Flex,
-  HStack,
-  Link,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Children, ReactNode } from "react";
+import { Box, Flex, HStack, Link, useColorModeValue } from "@chakra-ui/react";
 
-const Links = [ { 
-   "route": "/",
-   "text" : "Home" 
-},
-{
-   "route" : "/cars",
-   "text": "Cars" 
-}, 
-{
-    "route" : "/checkout",
-    "text" : "Checkout"
-} ];
+const Links = [
+  {
+    route: "/",
+    text: "Home",
+  },
+  {
+    route: "/cars",
+    text: "Cars",
+  },
+  {
+    route: "/checkout",
+    text: "Checkout",
+  },
+];
 
 const NavLink = ({ children }) => (
   <Link
     px={2}
     py={1}
-    rounded={'md'}
+    rounded={"md"}
     _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
     }}
-    href={children.route}>
+    href={children.route}
+  >
     {children.text}
   </Link>
 );
@@ -37,13 +34,14 @@ const NavLink = ({ children }) => (
 export default function NavigationBar() {
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <HStack spacing={8} alignItems={'center'}>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <HStack spacing={8} alignItems={"center"}>
             <HStack
-              as={'nav'}
+              as={"nav"}
               spacing={4}
-              display={{ base: 'none', md: 'flex' }}>
+              display={{ base: "none", md: "flex" }}
+            >
               {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
               ))}
--- cognizantgallery-web-client/src/pages/Home.js
+++ cognizantgallery-web-client/src/pages/Home.js
@@ -1,3 +1,58 @@
-export default function Home(){
-    return (<h1>Home</h1>);
+import {
+  Box,
+  Container,
+  Stack,
+  Heading,
+  Text,
+  Button,
+  Link,
+} from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <>
+      <Container maxW={"3xl"}>
+        <Stack
+          as={Box}
+          textAlign={"center"}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}
+          >
+            Welcome to the <br />
+            <Text as={"span"} color={"green.400"}>
+              test case!
+            </Text>
+          </Heading>
+          <Text color={"gray.500"}>
+            There is not much to see in this page acutally, you can proceed to
+            the cars page from here or navigation bar upwards.
+          </Text>
+          <Stack
+            direction={"column"}
+            spacing={3}
+            align={"center"}
+            alignSelf={"center"}
+            position={"relative"}
+          >
+            <Button
+              colorScheme={"green"}
+              bg={"green.400"}
+              rounded={"full"}
+              px={6}
+              _hover={{
+                bg: "green.500",
+              }}
+            >
+              <Link href="/cars">To The Cars!</Link>
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  );
 }
