0378604f02d5e5e7d825607d84fdedc80483ac2f
Tamer Berat Çelik <aducat5@hotmail.com>
2022-01-02 23:24:47 +0300

    added basic routing and navigation bar component.

--- cognizantgallery-web-client/src/components/NavigationBar.js
+++ cognizantgallery-web-client/src/components/NavigationBar.js
@@ -1,4 +1,4 @@
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function NavigationBar(){
     return (
@@ -8,7 +8,7 @@ export default function NavigationBar(){
               <Link to={"/"}>Home</Link>
             </li>
             <li>
-              <Link to={"/products"}>Products</Link>
+              <Link to={"/cars"}>Cars</Link>
             </li>
             <li>
               <Link to={"/checkout"}>Cart</Link>
